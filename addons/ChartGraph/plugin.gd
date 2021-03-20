82d1c0879cf3f96a5e9d7f870c9b89282b099b16
Chris Keddy <chrisskeddy@gmail.com>
2021-03-20 19:34:58 -0400

    setup svg icons

--- addons/ChartGraph/plugin.gd
+++ addons/ChartGraph/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 func _enter_tree():
 # Initialization of the plugin goes here
-	add_custom_type("ChartGraph", "ReferenceRect", load("res://addons/ChartGraph/plugin.gd"), preload("res://addons/ChartGraph/icon.png"))
+	add_custom_type("ChartGraph", "ReferenceRect", load("res://addons/ChartGraph/plugin.gd"), preload("res://addons/ChartGraph/icons/chartgraph_line.svg"))
 
 func _exit_tree():
 # Clean-up of the plugin goes here
